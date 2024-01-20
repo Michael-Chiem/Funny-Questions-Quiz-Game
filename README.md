aaaa9c0019464794521934f0b78d5ae73c2f2cf1
Michael Chiem <michaelchiem1@hotmail.com>
2024-01-20 11:46:24 -0700

    Updating README and Script.js file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Funny-Questions-Quiz-Game
-Timer Killer Quiz Game
+I created a Quiz Game using JavaScript.
+
+Credit to ChatGTP! I used ChatGTP to help fix error that I can't figure out.
+
+
+
--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-var quizData = 
+var quizData = [
+    {
+        question: "What do you call a fish with no eyes?",
+        option: ["Eyeless Fish", "Blidfish", "Noeye Trout", "Fish"],
+        correctAnswer: "Blidfish",
+    }
+];
+
+var currentQuestionIdex = 0;
+var score = 0;
+var playerName = '';
+var highestScores = [];
+
+var timer;
+var secondsPerQuestions = 40;
+
+var questionElement = document.getElementById('questions');
+var optionContainer = document.getElementById('options-container');
+var timerElement = document.getElementById('time');
+var scoreElemnt = document.getElementById('score');
+var playerNameInput = document.getElementById('player-name');
+var higheScoreTable = document.getElementById('high-score-table');
+var nextButton = document.getElementById('next-button');
+
+function startQuiz() {
+    currentQuestionIdex = 0;
+    score = 0;
+    playerName = playerNameInput.value || 'Your-Name-Go-Here';
+}
